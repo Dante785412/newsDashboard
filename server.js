938db2afe8ca09d14fe4e667a54c561b16080d91
dante785412 <gdigangiibb@web.de>
2020-04-22 17:15:02 +0200

    node server erstellt

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+require('dotenv').config();
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Node.js talking...');
+});
+
+
+server.listen(process.env.PORT, () => {
+    
+    console.log('Server listening at port ' + process.env.PORT);
+});
+
+
